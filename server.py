520685fbe8018fd91e5d3e0b424f40dc0eda22d7
cigraphics <cigraphics@gmail.com>
2013-04-21 05:12:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -9,38 +9,52 @@ import os
 call("/home/PiBits/ServoBlaster/servod")
 
 class DeltaBotHTTPRequestHandler(BaseHTTPRequestHandler):
-  def do_GET(self):
+	def do_GET(self):
 		url = self.path
 		url = url.replace("/", "").replace("favicon.ico", "").replace("?", "")
+		self.send_response(200)
+		self.send_header('Content-Type', 'text/html')
+		self.end_headers()
+		
 		if url == "" :
-			f = open('test.html')
-			self.send_response(200)
-			self.send_header('Content-Type', 'text/html')
-			self.end_headers()
+			url = "index.html"
+		
+		if url.endswith(".html") :
+			f = open(url)
 			self.wfile.write(f.read())
 			f.close()
 		else :
-			self.send_response(200)
-			self.send_header('Content-Type', 'text/html')
-			self.end_headers()
 			self.wfile.write('ok')
-			data = dict(urlparse.parse_qsl(url))
-			x = int(data["x"])
-			y = int(data["y"])
-			z = int(data["z"])
-			list = {x,y,z}
-			mx = max(list) + 1
-			nx = 0
-			ny = 0
-			nz = 0
-			for mx in range(1, mx) :
-				if nx < x :
-					nx = nx + 1
-				if ny < y :
-					ny = ny + 1
-				if nz < z :
-					nz = nz + 1
-				os.system("/home/project/delta.o " + str(nx) + " " + str(ny) + " " + str(nz))
+			if url == "reset" :
+				os.system("/home/project/reset.o")
+			else :
+				data = dict(urlparse.parse_qsl(url))
+				if data["z"] == "mouse" :
+					print("mouse")
+				else :
+					x = int(data["x"])
+					y = int(data["y"])
+					z = int(data["z"])
+					list = {x,y,z}
+					ma = max(list)
+					nx = 0
+					ny = 0
+					nz = 0
+					maZ = ma + 1
+					for i in range(0, maZ) :
+						if x == ma :
+							maxx = x
+						if y == ma :
+							maxx = y
+						if z == ma :
+							maxx = z
+						if maxx - x - i <= 0 and nx < x :
+							nx = nx + 1
+						if maxx - y - i <= 0 and ny < y :
+							ny = ny + 1
+						if maxx - z - i <= 0 and nz < z :
+							nz = nz + 1
+						os.system("/home/project/delta.o " + str(nx) + " " + str(ny) + " " + str(nz))
 		return
 
 def run():
